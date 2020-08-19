777c41d3cc26f98438bf92e9ce5205f9d34e3180
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2020-08-19 14:01:46 +0100

    ⚡ shorthand ParamTypes POC

--- app/client-app/src/app/pages/Editor/components/PageItemEditor/ParamsEditor.js
+++ app/client-app/src/app/pages/Editor/components/PageItemEditor/ParamsEditor.js
@@ -2,23 +2,55 @@ import React from "react";
 import Param from "./Param";
 import { Grid, Text } from "@chakra-ui/core";
 
+// TODO: move to ParamTypes?
+const convertArrayShorthands = (arr) => {
+  if (!Array.isArray(arr)) return arr;
+
+  let [type, label, defaultValue, oneOf] = arr;
+
+  // special cases
+  if (type === "string") defaultValue = defaultValue || "";
+  if (type === "string?") type = "string";
+
+  return {
+    type,
+    label,
+    defaultValue,
+    oneOf,
+  };
+};
+const convertStringShorthands = (type) => {
+  if (typeof type !== "string") return type;
+
+  return convertArrayShorthands([type]);
+};
+
 const ParamsEditor = ({ component, params, handleParamChange }) => {
   if (!component) return null;
 
   const list = [];
-  for (const key in component.params) {
-    const p = component.params[key];
-    if (!p) continue;
+  for (const key in component.paramTypes) {
+    let pt = component.paramTypes[key];
+    if (!pt) continue;
+
+    // we now care about what type of paramTypes entry this is
+
+    // we do some non destructive shorthand conversion attempts
+    pt = convertStringShorthands(pt);
+    pt = convertArrayShorthands(pt);
+
+    // if we're here, pt is a full ParamTypes object, or invalid ¯\_(ツ)_/¯
+
     list.push([
       <Text key={`${key}-label`} textAlign="right" fontWeight="bold">
-        {p.label}
+        {pt.label || key}
       </Text>,
       <Param
         key={`${key}-value`}
-        value={params?.[key] || p.defaultValue}
-        type={p.type}
+        value={params?.[key] || pt.defaultValue}
+        type={pt.type}
         paramKey={key}
-        oneOf={p.oneOf}
+        oneOf={pt.oneOf}
         onChange={handleParamChange}
       />,
     ]);
--- app/client-app/src/app/pages/Editor/components/PageItemEditor/ParamsEditor.stories.js
+++ app/client-app/src/app/pages/Editor/components/PageItemEditor/ParamsEditor.stories.js
@@ -2,12 +2,41 @@ import React from "react";
 import ParamsEditor from "./ParamsEditor";
 
 const Component = ({ text }) => <div>{text}</div>;
-Component.params = {
+Component.paramTypes = {
+  // object literal form
   text: {
     type: "string",
-    label: "Text",
-    defaultValue: "",
+    defaultValue: "lol",
   },
+  isBoolean: {
+    type: "bool",
+    label: "Is Boolean?",
+  },
+  howMany: {
+    type: "number",
+    label: "How Many?",
+  },
+  chooseOne: {
+    type: "oneOf",
+    label: "Choose One",
+    oneOf: ["Ham", "Cheese"],
+  },
+  // string shorthands
+  shorthandText: "string",
+  shorthandTextUndefined: "string?",
+  shorthandNumber: "number",
+  shorthandBool: "bool",
+  // array shorthands
+  arrText: ["string", "Array Shorthand Text", "default"],
+  arrTextUndefined: ["string?", "Array Shorthand Text (Undefined)"],
+  arrNumber: ["number", "Array Shorthand Number", 77],
+  arrBool: ["bool", "Array Shorthand Bool"],
+  arrOneOf: ["oneOf", "Array Shorthand OneOf", "Ham", ["Ham", "Cheese"]],
+  // or use ParamTypes builders <3
+
+  // array: {
+  //   type: "array",
+  // },
 };
 
 export default {
@@ -18,7 +47,7 @@ export default {
   },
   args: {
     component: Component,
-    params: [],
+    params: { isBoolean: false },
   },
 };
 
