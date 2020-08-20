79cf20eeda80c0e32603e490b6e9aba022dbab6e
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2020-08-20 10:19:40 +0100

    ⚡ Param Sections, Groups wip

--- app/client-app/src/app/pages/Editor/components/PageItemEditor/ParamsEditor.js
+++ app/client-app/src/app/pages/Editor/components/PageItemEditor/ParamsEditor.js
@@ -1,6 +1,17 @@
 import React from "react";
-import Param from "./Param";
-import { Grid, Text } from "@chakra-ui/core";
+import ParamControl from "./ParamControl";
+import {
+  Grid,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  FormControl,
+  FormLabel,
+  Stack,
+} from "@chakra-ui/core";
 
 // TODO: move to ParamTypes?
 const convertArrayShorthands = (arr) => {
@@ -19,53 +30,149 @@ const convertArrayShorthands = (arr) => {
     oneOf,
   };
 };
-const convertStringShorthands = (type) => {
-  if (typeof type !== "string") return type;
+const convertStringShorthands = (type) =>
+  typeof type !== "string" ? type : convertArrayShorthands([type]);
 
-  return convertArrayShorthands([type]);
+const buildControls = (_context, paramTypes, root = false) =>
+  Object.keys(paramTypes).reduce(
+    (result, key) => {
+      let pt = paramTypes[key];
+
+      if (!pt) return result;
+
+      // we do some non destructive shorthand conversion attempts
+      pt = convertStringShorthands(pt);
+      pt = convertArrayShorthands(pt);
+
+      // if we're here, pt is a full ParamTypes object, or invalid ¯\_(ツ)_/¯
+
+      if (pt.type === "section") {
+        // TODO: reference types enum
+        if (!root) {
+          console.error(
+            `DECSYS does not allow nested Parameter Type Sections at this time. Section: ${key}`
+          );
+          return result;
+        }
+
+        return {
+          ...result,
+          [key]: buildControls(_context, pt.paramTypes),
+        };
+      }
+
+      let controls;
+      if (pt.type === "group" || !pt.type) {
+        // TODO: reference types enum
+        if (!pt.type) pt = { paramTypes: pt };
+        controls = (
+          <Stack p={2} pt={0} bg="blackAlpha.200" borderRadius={10}>
+            <Text p={1} fontWeight="medium" borderBottom="thin solid">
+              {pt.label || key}
+            </Text>
+            {buildControls(_context, pt.paramTypes).controls},
+          </Stack>
+        );
+      } else {
+        controls = (
+          <ParamControlRow
+            key={key}
+            paramKey={key}
+            paramType={pt}
+            value={_context.params?.[key] ?? pt.defaultValue}
+            handleParamChange={_context.handleParamChange}
+          />
+        );
+      }
+
+      return {
+        ...result,
+        controls: [...result.controls, controls],
+      };
+    },
+    { controls: [] }
+  );
+
+const ParamControlRow = ({ value, paramType, paramKey, handleParamChange }) => {
+  const isLabelledType = paramType.type !== "bool";
+  return (
+    <ControlsGrid>
+      <FormLabel
+        id={`${paramKey}-label`}
+        textAlign="right"
+        fontWeight="medium"
+        color={isLabelledType ? "inherit" : "gray.400"}
+        mb={0}
+      >
+        {isLabelledType ? paramType.label || paramKey : " ~ "}
+      </FormLabel>
+
+      <ParamControl
+        value={value}
+        paramType={paramType}
+        paramKey={paramKey}
+        onChange={handleParamChange}
+      />
+    </ControlsGrid>
+  );
+};
+
+const ControlsGrid = (p) => (
+  <FormControl
+    as={Grid}
+    templateColumns="2fr 5fr"
+    gap={2}
+    alignItems="center"
+    width="100%"
+    {...p}
+  />
+);
+
+const EditorSections = ({ controls, ...sections }) => {
+  const sectionKeys = Object.keys(sections);
+
+  // handle 1 named section and no unparented params
+  // which should be rare...
+  if (!controls.length && sectionKeys.length === 1)
+    return sections[sectionKeys[0]].controls;
+
+  // multiple sections
+  return (
+    <Tabs size="sm" w="100%" gridColumn="span 2">
+      <TabList>
+        {sectionKeys.map((key) => (
+          <Tab>{key}</Tab>
+        ))}
+        {!!controls.length && <Tab>Other</Tab>}
+      </TabList>
+
+      <TabPanels>
+        {sectionKeys.map((key) => (
+          <TabPanel>{sections[key].controls}</TabPanel>
+        ))}
+        {controls.length && <TabPanel>{controls}</TabPanel>}
+      </TabPanels>
+    </Tabs>
+  );
 };
 
 const ParamsEditor = ({ component, params, handleParamChange }) => {
   if (!component) return null;
 
-  const list = [];
-  for (const key in component.paramTypes) {
-    let pt = component.paramTypes[key];
-    if (!pt) continue;
-
-    // we now care about what type of paramTypes entry this is
-
-    // we do some non destructive shorthand conversion attempts
-    pt = convertStringShorthands(pt);
-    pt = convertArrayShorthands(pt);
-
-    // if we're here, pt is a full ParamTypes object, or invalid ¯\_(ツ)_/¯
-
-    list.push([
-      <Text key={`${key}-label`} textAlign="right" fontWeight="bold">
-        {pt.label || key}
-      </Text>,
-      <Param
-        key={`${key}-value`}
-        value={params?.[key] || pt.defaultValue}
-        type={pt.type}
-        paramKey={key}
-        oneOf={pt.oneOf}
-        onChange={handleParamChange}
-      />,
-    ]);
-  }
+  const sections = buildControls(
+    { params, handleParamChange },
+    component.paramTypes,
+    true
+  );
 
   return (
-    <Grid
-      templateColumns="2fr 5fr"
-      gap={2}
-      alignItems="center"
-      width="100%"
-      p={2}
-    >
-      {list}
-    </Grid>
+    <Stack>
+      {Object.keys(sections).length > 1 ? (
+        <EditorSections {...sections} />
+      ) : (
+        sections.controls
+      )}
+    </Stack>
   );
 };
 
--- app/client-app/src/app/pages/Editor/components/PageItemEditor/ParamsEditor.stories.js
+++ app/client-app/src/app/pages/Editor/components/PageItemEditor/ParamsEditor.stories.js
@@ -3,37 +3,26 @@ import ParamsEditor from "./ParamsEditor";
 
 const Component = ({ text }) => <div>{text}</div>;
 Component.paramTypes = {
-  // object literal form
-  text: {
-    type: "string",
-    defaultValue: "lol",
+  param1: "string",
+  param2: "number",
+  group1: {
+    type: "group",
+    label: "Fully Specified Group",
+    paramTypes: {
+      groupParam1: "string",
+      groupParam2: "bool",
+    },
   },
-  isBoolean: {
-    type: "bool",
-    label: "Is Boolean?",
+  shorthandGroup: {
+    groupParam1: "number",
+    groupParam2: "string",
+    subGroup: {
+      type: "group",
+      paramTypes: { subParam1: ["oneOf", null, "Bread", ["Bread", "Water"]] },
+    },
   },
-  howMany: {
-    type: "number",
-    label: "How Many?",
-  },
-  chooseOne: {
-    type: "oneOf",
-    label: "Choose One",
-    oneOf: ["Ham", "Cheese"],
-  },
-  // string shorthands
-  shorthandText: "string",
-  shorthandTextUndefined: "string?",
-  shorthandNumber: "number",
-  shorthandBool: "bool",
-  // array shorthands
-  arrText: ["string", "Array Shorthand Text", "default"],
-  arrTextUndefined: ["string?", "Array Shorthand Text (Undefined)"],
-  arrNumber: ["number", "Array Shorthand Number", 77],
-  arrBool: ["bool", "Array Shorthand Bool"],
-  arrOneOf: ["oneOf", "Array Shorthand OneOf", "Ham", ["Ham", "Cheese"]],
-  // or use ParamTypes builders <3
 
+  //TODO:
   // array: {
   //   type: "array",
   // },
@@ -47,8 +36,143 @@ export default {
   },
   args: {
     component: Component,
-    params: { isBoolean: false },
   },
 };
 
-export const Basic = (args) => <ParamsEditor {...args} />;
+const Template = (args) => <ParamsEditor {...args} />;
+
+export const Basic = Template.bind({});
+
+export const ObjectLiterals = Template.bind({});
+ObjectLiterals.args = {
+  component: {
+    ...Component.bind({}),
+    paramTypes: {
+      text: {
+        type: "string",
+        defaultValue: "lol",
+      },
+      isBoolean: {
+        type: "bool",
+        label: "Is Boolean?",
+      },
+      howMany: {
+        type: "number",
+        label: "How Many?",
+      },
+      chooseOne: {
+        type: "oneOf",
+        label: "Choose One",
+        oneOf: ["Ham", "Cheese"],
+      },
+    },
+  },
+};
+
+export const StringShorthands = Template.bind({});
+StringShorthands.args = {
+  component: {
+    ...Component.bind({}),
+    paramTypes: {
+      shorthandText: "string",
+      shorthandTextUndefined: "string?",
+      shorthandNumber: "number",
+      shorthandBool: "bool",
+      // `oneOf` is impossible as it requires more information
+    },
+  },
+};
+
+export const ArrayShorthands = Template.bind({});
+ArrayShorthands.args = {
+  component: {
+    ...Component.bind({}),
+    paramTypes: {
+      arrText: ["string", "Array Shorthand Text", "default"],
+      arrTextUndefined: ["string?", "Array Shorthand Text (Undefined)"],
+      arrNumber: ["number", "Array Shorthand Number", 77],
+      arrBool: ["bool", "Array Shorthand Bool"],
+      arrOneOf: ["oneOf", "Array Shorthand OneOf", "Ham", ["Ham", "Cheese"]],
+    },
+  },
+};
+
+export const OneSectionOnly = Template.bind({});
+OneSectionOnly.args = {
+  component: {
+    ...Component.bind({}),
+    paramTypes: {
+      "Section 1": {
+        type: "section",
+        paramTypes: {
+          section1Param: "string",
+        },
+      },
+    },
+  },
+};
+
+export const OneSectionPlusUnparented = Template.bind({});
+OneSectionPlusUnparented.args = {
+  component: {
+    ...Component.bind({}),
+    paramTypes: {
+      unparentedParam: "string",
+      ...OneSectionOnly.args.component.paramTypes,
+    },
+  },
+};
+
+export const MultipleSections = Template.bind({});
+MultipleSections.args = {
+  component: {
+    ...Component.bind({}),
+    paramTypes: {
+      ...OneSectionOnly.args.component.paramTypes,
+      "Section 2": {
+        type: "section",
+        paramTypes: {
+          section2Param: "bool",
+        },
+      },
+    },
+  },
+};
+
+export const MultipleSectionsPlusUnparented = Template.bind({});
+MultipleSectionsPlusUnparented.args = {
+  component: {
+    ...Component.bind({}),
+    paramTypes: {
+      unparentedParam: "string",
+      ...MultipleSections.args.component.paramTypes,
+    },
+  },
+};
+
+export const Groups = Template.bind({});
+Groups.args = {
+  component: {
+    ...Component.bind({}),
+    paramTypes: {
+      param1: "string",
+      group1: {
+        type: "group",
+        label: "Fully Specified Group",
+        paramTypes: {
+          groupParam1: "string",
+          groupParam2: "bool",
+        },
+      },
+      shorthandGroup: {
+        groupParam1: "number",
+        subGroup: {
+          type: "group",
+          paramTypes: {
+            subParam1: ["oneOf", null, "Bread", ["Bread", "Water"]],
+          },
+        },
+      },
+    },
+  },
+};
